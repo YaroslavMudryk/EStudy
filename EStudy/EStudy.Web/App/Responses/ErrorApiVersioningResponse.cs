ea5dea2b0608052396081f5f13bcae81bb5b75f5
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-01 12:42:36 +0300

    Added versioning

---
+++ EStudy/EStudy.Web/App/Responses/ErrorApiVersioningResponse.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
+namespace EStudy.Web.App.Responses
+{
+    public class ErrorApiVersioningResponse : IErrorResponseProvider
+    {
+        public IActionResult CreateResponse(ErrorResponseContext context)
+        {
+            return new BadRequestObjectResult(new { error = context.Message, desc = context.MessageDetail });
+        }
+    }
+}
---
+++ EStudy/EStudy.Web/Controllers/API/ApiBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace EStudy.Web.Controllers.API
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ApiBaseController : ControllerBase
+    {
+
+    }
+}
