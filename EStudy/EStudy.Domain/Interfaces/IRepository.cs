e2059b3594e2037c591f042cb9c3e7b82f99e967
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-01 13:23:59 +0300

    Added domain project

---
+++ EStudy/EStudy.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using EStudy.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStudy.Domain.Interfaces
+{
+    public interface IRepository<TEntity, Id> where TEntity : BaseModel<Id>
+    {
+        Task<TEntity> CreateAsync(TEntity entity);
+        Task CreateAsync(List<TEntity> entities);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task UpdateAsync(List<TEntity> entities);
+        Task<TEntity> RemoveAsync(TEntity entity);
+        Task RemoveRangeAsync(List<TEntity> entities);
+        Task<int> CountAsync();
+        Task<int> CountAsync(Expression<Func<TEntity, bool>> match);
+        Task<List<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> match);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> match);
+        Task<TEntity> GetByIdAsync(Id id);
+        Task<List<TEntity>> GetAllAsync(int offset, int count);
+    }
+}
---
+++ EStudy/EStudy.Domain/Models/University/University.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EStudy.Domain.Models.University
+{
+    public class University : BaseModel<int>
+    {
+        [Required, StringLength(300, MinimumLength = 5)]
+        public string Name { get; set; }
+        [StringLength(300)]
+        public string Logo { get; set; }
+        public string CodeEDEBO { get; set; }
+        [Required, StringLength(30, MinimumLength = 2)]
+        public string ShortName { get; set; }
+        [Required]
+        public string EnName { get; set; }
+        [StringLength(12)]
+        public string IdentityCode { get; set; }
+        [StringLength(50)]
+        public string Type { get; set; }
+        [StringLength(100)]
+        public string Owner { get; set; }
+        [StringLength(250)]
+        public string DependsOn { get; set; }
+        [StringLength(250)]
+        public string Rector { get; set; }
+        [StringLength(250)]
+        public string Address { get; set; }
+        [StringLength(15, MinimumLength = 7)]
+        public string Phone { get; set; }
+        [StringLength(100, MinimumLength = 5)]
+        public string Email { get; set; }
+        [StringLength(250)]
+        public string Photo { get; set; }
+        [StringLength(5000)]
+        public string History { get; set; }
+        [StringLength(15)]
+        public string YearOfFoundation { get; set; }
+    }
+}
