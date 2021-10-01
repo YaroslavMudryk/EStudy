377d2eb6cd63d55070b9173a5a0b42ef30f64f13
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-01 13:46:44 +0300

    Added Chair model

--- EStudy/EStudy.Domain/Models/University/Chair.cs
+++ EStudy/EStudy.Domain/Models/University/Chair.cs
@@ -1,20 +1,18 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace EStudy.Domain.Models.University
 {
-    public class Institute : BaseModel<int>
+    public class Chair : BaseModel<int>
     {
         [Required, StringLength(150, MinimumLength = 5)]
         public string Name { get; set; }
         [Required, StringLength(150, MinimumLength = 5)]
         public string EnName { get; set; }
-        [StringLength(5000)]
-        public string About { get; set; }
+        [StringLength(250)]
+        public string Logo { get; set; }
+        [StringLength(20000)]
+        public string AboutPayload { get; set; }
         [StringLength(2500)]
         public string ManagementPayload { get; set; }
-        [StringLength(2500)]
-        public string AboutPayload { get; set; }
-        public List<Chair> Chairs { get; set; }
     }
 }
