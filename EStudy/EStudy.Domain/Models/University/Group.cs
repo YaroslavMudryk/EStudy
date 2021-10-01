14436b3b6ae017af4c7ea28836ba14914d85bf63
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-01 14:00:04 +0300

    Added Specialty and Group models

---
+++ EStudy/EStudy.Domain/Models/University/Group.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EStudy.Domain.Models.University
+{
+    public class Group : BaseModel<int>
+    {
+        [Required, StringLength(150, MinimumLength = 3)]
+        public string Name { get; set; }
+        [Required]
+        public int Course { get; set; }
+        [Required, StringLength(6)]
+        public string CodeConnect { get; set; }
+        [Required, StringLength(25, MinimumLength = 25)]
+        public string InviteLink { get; set; }
+        public string ManagmentPayload { get; set; }
+    }
+}
--- EStudy/EStudy.Domain/Models/University/Specialty.cs
+++ EStudy/EStudy.Domain/Models/University/Specialty.cs
@@ -3,18 +3,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EStudy.Domain.Models.University
 {
-    public class Chair : BaseModel<int>
+    public class Specialty : BaseModel<int>
     {
         [Required, StringLength(150, MinimumLength = 5)]
         public string Name { get; set; }
         [Required, StringLength(150, MinimumLength = 5)]
         public string EnName { get; set; }
-        [StringLength(250)]
-        public string Logo { get; set; }
-        [StringLength(20000)]
-        public string AboutPayload { get; set; }
-        [StringLength(2500)]
-        public string ManagementPayload { get; set; }
-        public List<Specialty> Specialties { get; set; }
+        [StringLength(1000, MinimumLength = 5)]
+        public string Info { get; set; }
+        [StringLength(1000, MinimumLength = 5)]
+        public string PdfDocument { get; set; }
+        public List<Group> Groups { get; set; }
     }
 }
